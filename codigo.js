bcc7b23cef574032b59a918fe97e043dcc6f36d3
Marcos Brito <54248009+mabrito2019@users.noreply.github.com>
2022-05-08 11:18:51 -0300

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -1,3 +1,5 @@
+
+
 const dadosQuebrados= require('./broken-database.json'); 
 
 let bancoCerto = dadosQuebrados;
@@ -21,10 +23,18 @@ for (var i = 0; i < bancoCerto.length; i++){
 bancoCerto[i].name = updateChar(bancoCerto[i].name);
 }
 
-// converte o atribulo Price para tipo number float com duas casas decimais 
+// converte o atribulo Price para tipo number float com duas casas decimais.
 for (var i = 0; i < bancoCerto.length; i++){
     bancoCerto[i].price = parseFloat(bancoCerto[i].price).toFixed(2);
     }
 
+//Verificador de Propriedade
 
+for (var i = 0; i < bancoCerto.length; i++){
+if (!bancoCerto[i].hasOwnProperty('quantity')){
+    
+    bancoCerto[i].quantity= 0;
+    console.log(bancoCerto[i]);
+}
+}
 console.log(bancoCerto);
